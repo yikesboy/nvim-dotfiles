ce081e45d69f73942eda0d1cfe745d6fbe42beae
JL <115737916+yikesboy@users.noreply.github.com>
2025-01-24 18:34:14 +0100

    feat: added custom mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Center view after Ctrl+d and Ctrl+u
+map("n","<C-d>", "<C-d>zz",{ desc = "Scroll down and center"})
+map("n","<C-u>", "<C-u>zz",{ desc = "Scroll up and center"})
+
+-- Center view after n and N in search
+map("n", "n", "nzz", { desc = "Next search result and center" })
+map("n", "N", "Nzz", { desc = "Previous search result and center" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
